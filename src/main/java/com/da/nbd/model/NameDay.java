bcb033192e107761b2f8fc517c0e56b2cc5d7c72
dokaattila <dokaatti@gmail.com>
2022-04-19 18:19:53 +0200

    remove unnecessarily annotations

--- src/main/java/com/da/nbd/model/NameDay.java
+++ src/main/java/com/da/nbd/model/NameDay.java
@@ -2,13 +2,9 @@ package com.da.nbd.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.extern.jackson.Jacksonized;
-
-import java.util.List;
 
 @AllArgsConstructor
 @Data
-@Jacksonized
 public class NameDay {
 
     private int id;
