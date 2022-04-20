db2a7ab474a7abae1b5f4d3e2191c0b83322b9e4
dokaattila <dokaatti@gmail.com>
2022-04-20 19:35:04 +0200

    remove one unnecessarily annotation

--- src/main/java/com/da/nbd/App.java
+++ src/main/java/com/da/nbd/App.java
@@ -2,9 +2,9 @@ package com.da.nbd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+
+@SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
