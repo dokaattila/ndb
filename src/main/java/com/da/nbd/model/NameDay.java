aea2d474b9cfa55bdb32ca28d09d70045b014e95
dokaattila <dokaatti@gmail.com>
2022-04-20 19:31:13 +0200

    add required annotations

--- src/main/java/com/da/nbd/model/NameDay.java
+++ src/main/java/com/da/nbd/model/NameDay.java
@@ -2,19 +2,24 @@ package com.da.nbd.model;
 
 import lombok.*;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @AllArgsConstructor
 @Getter
 @Setter
 @NoArgsConstructor
-@Entity
+@Entity(name = "nameday")
+@Table(name = "namedays")
+
 public class NameDay {
 
     @Id
-    private int id;
+    private Integer id;
     private String names;
+    @Column(name = "date")
     private String monthDay;
 
 
